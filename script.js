e3b5403566433bf7a17c1df314b1a51efcca3ac7
Pratikjoshi9529 <120738524+Pratikjoshi9529@users.noreply.github.com>
2023-05-14 08:23:00 -0400

    Initial

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('startbtn').addEventListener('click', start);
+document.getElementById('stopbtn').addEventListener('click', stop);
+document.getElementById('resetbtn').addEventListener('click', reset);
+let interval;
+function start() {
+    clearInterval(interval);
+    interval = setInterval(startTimer, 10);
+}
+function stop() {
+    clearInterval(interval);
+}
+function reset() {
+    document.getElementById("ms").innerText = "00";
+    document.getElementById("sec").innerText = "00";
+    document.getElementById("min").innerText = "00";
+    // clearInterval(interval);
+}
+function startTimer() {
+    document.getElementById("ms").innerText = parseInt(document.getElementById("ms").innerText) + 1;
+    if (parseInt(document.getElementById("ms").innerText) >= 100) {
+        document.getElementById("sec").innerText = parseInt(document.getElementById("sec").innerText) + 1;
+        document.getElementById("ms").innerText = "00";
+        if (parseInt(document.getElementById("sec").innerText) >= 60) {
+            document.getElementById("min").innerText = parseInt(document.getElementById("min").innerText) + 1;
+            document.getElementById("sec").innerText = "00";
+        }
+    }
+}
